ce3e411f4cddc485cc9d0aae1dba601837761372
Jonathan Rodríguez <jonathan.rodriguez@roiback.com>
2017-11-30 14:00:04 +0100

    incrementing jobs quotes

--- src/workers/listeners/page.js
+++ src/workers/listeners/page.js
@@ -8,17 +8,17 @@ const {
 const queue = kue.createQueue();
 
 
-queue.process('accumulateMetricsPageVisit', 2, (job, done) => {
+queue.process('accumulateMetricsPageVisit', 50, (job, done) => {
     //call job implementation with required parameters.
     accumulateMetricsPageVisit(job.data, done);
 });
 
-queue.process('accumulateMetricsPageActions', 2, (job, done) => {
+queue.process('accumulateMetricsPageActions', 50, (job, done) => {
     //call job implementation with required parameters.
     accumulateMetricsPageActions(job.data, done);
 });
 
-queue.process('accumulateMetricsBookings', 2, (job, done) => {
+queue.process('accumulateMetricsBookings', 50, (job, done) => {
     //call job implementation with required parameters.
     accumulateMetricsBookings(job.data, done);
 });
